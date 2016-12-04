1f26bf61ca13562fe175b66fa29bcd2fb38b8ebc
shibataka000 <chise.alter.pasta@gmail.com>
2016-12-04 22:12:36 +0900

    Use "map" and "filter" instead of "for" in problem004.

--- problems/problem004.py
+++ problems/problem004.py
@@ -10,10 +10,7 @@ def is_palindromic_number(x):
 
 
 def solve():
-    palindromic_numbers = []
-    seq = range(100, 1000)
-    for (a, b) in itertools.combinations(seq, 2):
-        n = a * b
-        if is_palindromic_number(n):
-            palindromic_numbers.append(n)
-    return max(palindromic_numbers)
+    seq = itertools.combinations(range(100, 1000), 2)
+    seq = map(lambda x: x[0] * x[1], seq)
+    seq = filter(is_palindromic_number, seq)
+    return max(seq)
