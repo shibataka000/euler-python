f8a18f31e91df4309aaf2efd934fd074b81482e8
shibataka000 <chise.alter.pasta@gmail.com>
2016-12-31 22:40:54 +0900

    Use math module to calculate factorial.

--- problems/problem015.py
+++ problems/problem015.py
@@ -1,12 +1,8 @@
 # coding: utf-8
 
-import math_ext
-
-
-def factorial(n):
-    return math_ext.product(range(1, n + 1))
+import math
 
 
 def solve():
     n = 20
-    return int(factorial(n * 2) / (factorial(n) * factorial(n)))
+    return int(math.factorial(n * 2) / (math.factorial(n) * math.factorial(n)))
