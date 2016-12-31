bd5754afaa630ff66d510cd9f2381216f481bc89
shibataka000 <chise.alter.pasta@gmail.com>
2016-12-31 22:41:03 +0900

    Solve problem016.

---
+++ problems/problem016.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+
+
+def solve():
+    return sum([int(x) for x in str(2 ** 1000)])
---
+++ problems/tests/test_problem016.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+import problems.problem016 as problem
+
+
+def test_solve():
+    assert problem.solve() == 1366
