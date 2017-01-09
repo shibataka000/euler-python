2f0eb609d5e8b7bce606455c0099fb4f3bd736d7
shibataka000 <chise.alter.pasta@gmail.com>
2017-01-09 15:20:01 +0900

    Move data in problem067.

--- problems/problem067.py
+++ problems/problem067.py
@@ -4,6 +4,6 @@ from . import problem018
 
 
 def solve():
-    with open("./problems/datas/problem067_triangle.txt") as f:
+    with open("./problems/datas/problem067/triangle.txt") as f:
         triangle = f.read()
     return problem018.get_max_route_value(triangle)
