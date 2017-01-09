e5808622017e5b32c815a96cfc572c4661440aad
shibataka000 <chise.alter.pasta@gmail.com>
2017-01-09 12:26:47 +0900

    Solve problem020.

---
+++ problems/problem020.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+import math
+
+
+def solve():
+    return sum([int(c) for c in str(math.factorial(100))])
---
+++ problems/tests/test_problem020.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+import problems.problem020 as problem
+
+
+def test_solve():
+    assert problem.solve() == 648
