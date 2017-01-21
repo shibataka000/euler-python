8409a6aafe3dd55f34d6979f4d2ece656d34698b
shibataka000 <chise.alter.pasta@gmail.com>
2017-01-21 23:05:41 +0900

    Solve problem034 more efficiently.

--- problems/problem034.py
+++ problems/problem034.py
@@ -1,12 +1,19 @@
 # coding: utf-8
 
+import itertools
 import math
 
 
-def sum_factorial(n):
-    return sum([math.factorial(int(x)) for x in str(n)])
-
-
 def solve():
-    return sum([i for i in range(3, 10 ** 7)
-                if sum_factorial(i) == i])
+    ans = []
+    for digits in itertools.combinations_with_replacement(range(10), 7):
+        sum_of_factorial = sum([math.factorial(x) for x in digits if x != 0])
+        for i in range(digits.count(0)):
+            a = sum_of_factorial + i
+            b = sum([math.factorial(int(x)) for x in str(a)])
+            if a == b:
+                ans.append(a)
+    ans = list(set(ans))
+    ans.remove(1)
+    ans.remove(2)
+    return sum(ans)
--- problems/tests/test_problem034.py
+++ problems/tests/test_problem034.py
@@ -3,9 +3,5 @@
 import problems.problem034 as problem
 
 
-def test_sum_factorial():
-    assert problem.sum_factorial(145) == 145
-
-
 def test_solve():
     assert problem.solve() == 40730
