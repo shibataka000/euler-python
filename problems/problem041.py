687a031b29ea5c287cf9d2ed40783abf4e492822
shibataka000 <chise.alter.pasta@gmail.com>
2017-03-10 23:22:09 +0900

    Modify problem041 to use itertools.

--- problems/problem041.py
+++ problems/problem041.py
@@ -1,10 +1,15 @@
 # coding: utf-8
 
+import itertools
+
 import math_ext
+import itertools_ext
 
 
 def solve():
-    for d in range(9, 0, -1):
-        for p in reversed(sorted(math_ext.pandigital(d))):
-            if math_ext.is_prime(p):
-                return p
+    seq = range(9, 0, -1)
+    seq = map(math_ext.pandigital, seq)
+    seq = map(sorted, seq)
+    seq = map(reversed, seq)
+    seq = itertools.chain.from_iterable(seq)
+    return itertools_ext.find(math_ext.is_prime, seq)
