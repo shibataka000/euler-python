f340810d66a931d8f7543e350a691dfd31ddabd8
shibataka000 <chise.alter.pasta@gmail.com>
2017-03-12 16:07:31 +0900

    Fix bug on 5df5af95686d3405ce47ebae9b6382216072af7f .

--- math_ext/tests/test_math_ext.py
+++ math_ext/tests/test_math_ext.py
@@ -78,7 +78,7 @@ def test_pandigital():
               2134, 2143, 2314, 2341, 2413, 2431,
               3124, 3142, 3214, 3241, 3412, 3421,
               4123, 4132, 4213, 4231, 4312, 4321]
-    actual = list(math_ext.pandigital(range(1, 4)))
+    actual = list(math_ext.pandigital(range(1, 5)))
     assert expect == actual
 
 
